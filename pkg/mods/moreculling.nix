243dccbb3158a54ebec68761cb3f1a929de30a8d
SharpStormGames <SharpStormGames@users.noreply.github.com>
2024-10-25 18:35:58 +0000

    Update Metadata

--- pkg/mods/moreculling.nix
+++ pkg/mods/moreculling.nix
@@ -18,7 +18,7 @@
   sha512 = "ab1583b335c631d0ee3fdbe98091fbdb26d1dbe9d32ea7e8a0874b75544629af607477a3d1cd38e92a2c3ff0c928eeedcac11d6d085182b60c1f9e7798ddb854";
 
   meta = {
-    gameVersions = [ ''1.21.1'' ];
+    gameVersions = [ ''1.21'' ''1.21.1'' ];
     loaders = [ ''fabric'' ''quilt'' ];
   };
 };
